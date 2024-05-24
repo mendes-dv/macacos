23d0831c05cbe0c4e8e4c46fa3ae08cb59e2f0f9
goncalo.mendes <goncalo.mendes@izertis.com>
2024-05-24 18:27:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import { defineComponent, ref, onMounted } from 'vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import XMap from "@/components/XMap.vue";
+
+let greenIcon = L.icon({
+  iconUrl: 'leaf-green.png',
+  shadowUrl: 'leaf-shadow.png',
+
+  iconSize:     [38, 95], // size of the icon
+  shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+export default defineComponent({
+  name: 'MapPage',
+  components: {
+    HelloWorld,
+    XMap
+  },
+  setup () {
+    const markers = ref([
+      { uid: 1, latitude: -26.8560346, longitude: -49.239189, descricao: "Marker 1", },
+      { uid: 2, latitude: -26.5, longitude: -49, descricao: "Marker 2" }
+    ])
+    const markersOverLay = ref([
+      { uid: 1, latitude: -22.8560346, longitude: -49.239189, descricao: "Marker 3", },
+      { uid: 2, latitude: -23.5, longitude: -49, descricao: "Marker 4" }
+    ])
+    const loading = ref(true)
+
+    return {
+      markers,
+      loading,
+      markersOverLay
+    }
+  }
+})
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <div>ehrwhrk</div>
+    <XMap
+        :markers="markers"
+        :markers-overlay="markersOverLay"
+    />
+  </main>
+</template>
+
---
+++ src/components/XMap.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div id="mapContainer" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, onBeforeMount } from 'vue'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+export default defineComponent({
+  name: 'LMap',
+  props: {
+    markers: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    markersOverlay: {
+      type: Array,
+      required: false,
+      default: () => []
+    }
+  },
+  setup (props) {
+    let map = null
+
+    onMounted(() => {
+      createMapLayer()
+    })
+
+    onBeforeMount(() => {
+      if (map) {
+        map.remove()
+      }
+    })
+
+    const createMapLayer = () => {
+      map = L.map('mapContainer').setView([-26.8560346, -49.239189], 5)
+
+      // Base layers
+      const osmBaseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+      })
+
+      const googleBaseLayer = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: '&copy; Google Maps'
+      })
+
+      // Overlays
+      const markerLayer = L.layerGroup().addTo(map)
+      const markersOverlay = L.layerGroup().addTo(map)
+
+      // Add markers to the markerLayer
+      if (props.markers.length) {
+        setMarkers(markerLayer, props.markers)
+      }
+
+      if (props.markersOverlay.length) {
+        setMarkers(markersOverlay, props.markersOverlay)
+      }
+
+      // Add base layers and overlays to the map
+      L.control.layers({
+        'Google Maps': googleBaseLayer
+      }, {
+        'AgricultorA': markerLayer,
+        'AgricultorB': markersOverlay
+      }).addTo(map)
+    }
+
+    const setMarkers = (markerLayer, markers) => {
+      markers.forEach((marker) => {
+        L.marker([marker.latitude, marker.longitude]).addTo(markerLayer)
+            .bindPopup(marker.descricao)
+      })
+    }
+  }
+})
+</script>
+
+<style scoped>
+#mapContainer {
+  width: 100vw;
+  height: calc(100vh - 50px);
+}
+</style>
